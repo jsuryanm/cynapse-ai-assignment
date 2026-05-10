7d04d4dc9493232af58711828f59fbe97a9ae4d4
Jayasuryan Mutyala <jsm.dgme@gmail.com>
2026-05-10 12:53:55 +0800

    feat: building the face detector component using InsightFace SRCD model

--- src/constants.py
+++ src/constants.py
@@ -8,7 +8,11 @@ HEAD_KPT_INDICES = (0,1,2,3,4)
 TORSO_KPT_INDICES =  (5,6,11,12)
 LEG_KPT_INDICES = (13,14,15,16)
 
-if __name__ == '__main__':
-    print(PROJECT_ROOT_DIR)
-    print(DATA_DIR)
-    print(IMAGES_DIR)
+LANDMARK_NAMES = ["right_eye", "left_eye", "nose", "right_mouth", "left_mouth"]
+COLORS = ["red", "blue", "green", "orange", "purple"]
+
+
+# if __name__ == '__main__':
+#     print(PROJECT_ROOT_DIR)
+#     print(DATA_DIR)
+#     print(IMAGES_DIR)
